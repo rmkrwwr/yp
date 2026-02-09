a57ad778b25bb9a4f609c709b0b25c80d96f47f6
rmkrwwr <rmkrwwr@gmail.com>
2026-02-09 21:01:46 +0300

    11

--- catalog/views.py
+++ catalog/views.py
@@ -1,78 +1,74 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+COURSES = [
+    {'id': 1, 'title': 'Python для начинающих', 'description': 'Основы Python'},
+    {'id': 2, 'title': 'Django Pro', 'description': 'Веб-разработка на Django'},
+    {'id': 3, 'title': 'Frontend с нуля', 'description': 'HTML, CSS, JavaScript'},
+]
+
+AUTHORS = [
+    {'id': 1, 'name': 'Витя Ламба', 'bio': 'Python разработчик'},
+    {'id': 2, 'name': 'Андрей Падалко', 'bio': 'Django разработчик'},
+    {'id': 3, 'name': 'Семен Мельников', 'bio': 'Frontend разработчик'},
+]
+
 def index(request):
     name = request.GET.get('name', 'Гость')
     password = request.GET.get('password', 'Не задан')
-    return HttpResponse(f"""
-    <h1>Стартовая страница</h1>
-    <p>Name: {name}</p>
-    <p>Password: {password}</p>
-    """)
+
+    context = {
+        'name': name,
+        'password': password,
+        'courses_count': len(COURSES),
+        'authors_count': len(AUTHORS),
+    }
+    return render(request, 'index.html', context)
 
 def courses(request):
-    courses_list = """
-    <h1>Список курсов</h1>
-    <ul>
-        <li>Python для начинающих</li>
-        <li>Django Pro</li>
-        <li>Frontend с нуля</li>
-    </ul>
-    <a href="/">На главную</a>
-    """
-    return HttpResponse(courses_list)
+    context = {
+        'courses': COURSES,
+    }
+    return render(request, 'courses.html', context)
 
 def course_detail(request, course_id):
-    return HttpResponse(f"""
-    <h1>Курс #{course_id}</h1>
-    <p>Информация о курсе с ID {course_id}</p>
-    <a href="/courses/">Назад к курсам</a>
-    """)
+    course = next((c for c in COURSES if c['id'] == course_id), None)
+
+    if not course:
+        return render(request, 'not_found.html', status=404)
+
+    context = {
+        'course': course,
+    }
+    return render(request, 'course_detail.html', context)
 
 def authors(request):
-    authors_list = """
-    <h1>Список авторов</h1>
-    <ul>
-        <li><a href="/author/1/">Витя Ламба</a></li>
-        <li><a href="/author/2/">Андрей Падалко</a></li>
-        <li><a href="/author/3/">Семен Мельников</a></li>
-    </ul>
-    <a href="/">На главную</a>
-    """
-    return HttpResponse(authors_list)
+    context = {
+        'authors': AUTHORS,
+    }
+    return render(request, 'authors.html', context)
 
 def author_detail(request, author_id):
-    return HttpResponse(f"""
-    <h1>Автор #{author_id}</h1>
-    <p>Профиль автора с ID {author_id}</p>
-    <a href="/authors/">Назад к авторам</a>
-    """)
-def info(request):
-    host = request.META.get("HTTP_HOST", "Не указан")
-    user_agent = request.META.get("HTTP_USER_AGENT", "Не указан")
-    path = request.path
-    method = request.method
-    cookies = request.COOKIES
+    author = next((a for a in AUTHORS if a['id'] == author_id), None)
 
-    return HttpResponse(f"""
-    <h1>Информация о запросе</h1>
-    <p>Host: {host}</p>
-    <p>User Agent: {user_agent}</p>
-    <p>Path: {path}</p>
-    <p>Method: {method}</p>
-    <p>Cookies: {cookies}</p>
-    <a href="/">На главную</a>
-    """)
-def not_found(request, exception):
-    return HttpResponse(f"""
-    <h1>404 - Страница не найдена хахахах</h1>
-    <p>Такой страницы не существует</p>
-    <p>Ошибка: {exception}</p>
-    <a href="/">На главную</a>
-    """, status=404)
-def test_404(request):
-    return HttpResponse("""
-    <h1>Тестовая 404 страница</h1>
-    <p>Это для проверки</p>
-    <a href="/">На главную</a>
-    """)
+    if not author:
+        return render(request, 'not_found.html', status=404)
+
+    context = {
+        'author': author,
+    }
+    return render(request, 'author_detail.html', context)
+
+def info(request):
+    context = {
+        'host': request.META.get("HTTP_HOST", "Не указан"),
+        'user_agent': request.META.get("HTTP_USER_AGENT", "Не указан"),
+        'path': request.path,
+        'method': request.method,
+        'cookies': request.COOKIES,
+        'get_params': request.GET,
+    }
+    return render(request, 'info.html', context)
+
+def not_found(request, exception=None):
+    return render(request, 'not_found.html', status=404)
